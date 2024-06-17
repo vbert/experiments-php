9920887e2524ea145596f53fb7444db4fcb1d240
Wojciech Sobczak <wsobczak@gmail.com>
2024-06-18 00:55:36 +0200

    Refactoring code

--- date-operations/example.php
+++ date-operations/example.php
@@ -5,7 +5,7 @@
  * File Created: 2024-06-17, 11:39:33
  * Author: Wojciech Sobczak (wsobczak@gmail.com)
  * -----
- * Last Modified: 2024-06-17, 11:48:48
+ * Last Modified: 2024-06-17, 20:57:26
  * Modified By: Wojciech Sobczak (wsobczak@gmail.com)
  * -----
  * Copyright © 2021 - 2024 by vbert
@@ -54,14 +54,17 @@ for ($i = 0; $i < 7; $i++) {
 <body>
     <div>
         <h1>Tygodnie</h1>
-        <p>Numer tygodnia: <?php echo $weekNumber; ?></p>
-        <p>Pierwszy dzień: <?php echo $firstDayOfWeek->format('Y-m-d'); ?></p>
-        <p>Ostatni dzień: <?php echo $lastDayOfWeek->format('Y-m-d'); ?></p>
-        <p>Dni tygodnia: <?php echo implode(', ', $daysOfWeek); ?></p>
+        <p>WeekOffset: <?= $weekOffset; ?></p>
+        <p>Numer tygodnia: <?=$weekNumber; ?></p>
+        <p>Pierwszy dzień: <?=$firstDayOfWeek->format('Y-m-d'); ?></p>
+        <p>Ostatni dzień: <?=$lastDayOfWeek->format('Y-m-d'); ?></p>
+        <p>Dni tygodnia: <?=implode(', ', $daysOfWeek); ?></p>
     </div>
     <form method="post">
-        <button type="submit" name="week_offset" value="<?php echo $weekOffset - 1; ?>">Poprzedni tydzień</button>
-        <button type="submit" name="week_offset" value="<?php echo $weekOffset + 1; ?>">Następny tydzień</button>
+        <button type="submit" name="week_offset" value="<?=$weekOffset - 1; ?>">
+            Poprzedni tydzień</button>
+        <button type="submit" name="week_offset" value="<?=$weekOffset + 1; ?>">
+            Następny tydzień</button>
     </form>
 </body>
 </html>
